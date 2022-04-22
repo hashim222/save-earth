06c8f7abe7678dffe14b44e6af76ffba48b658dc
Anthony O'Brien <aux.fuse@yahoo.com>
2022-04-22 23:19:02 +0100

    proof of concept added with model-viewer module from Google and Mars planet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
 //test to check js linked
 console.log("test");
+
+const modelViewer1 = document.querySelector("#mars-viewer");
+const annotationClicked = (annotation) => {
+    let dataset = annotation.dataset;
+    modelViewer1.cameraTarget = dataset.target;
+    modelViewer1.cameraOrbit = dataset.orbit;
+}
+
+modelViewer1.querySelectorAll('button').forEach((hotspot) => {
+    console.log("hello hotspot", hotspot);
+    hotspot.addEventListener('click', () => annotationClicked(hotspot));
+});
