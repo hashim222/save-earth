9c888f47554257b701c6c176ed91ddb72e3c41de
Anthony O'Brien <aux.fuse@yahoo.com>
2022-04-24 13:08:55 +0100

    Landing defined with appropriate html, css, js and click handlers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
 /***
  * Model Viewer integration for Annotations
  */
+const landing = document.getElementById("landing");
+const landingViewer = document.getElementById("landing-viewer");
+
+if(landingViewer) {
+    
+    let modelSrc = landingViewer.getAttribute(landingViewer.getAttribute('src'));
+
+    landingViewer.addEventListener("click", () => {
+        switch (modelSrc) {
+            case 'assets/models/earth/earth-model.gltf':
+                landingViewer.setAttribute(
+                    'src', 'assets/models/mars/mars-model.gltf'
+                );
+                break;
+            case 'assets/models/mars/mars-model.gltf':
+                landingViewer.setAttribute(
+                    'src', 'assets/models/earth/earth-model.gltf'
+                );
+                break;
+            default:
+                landingViewer.setAttribute(
+                    'src', 'assets/models/mars/mars-model.gltf'
+                );
+        }
+    })
+}
+
 const modelViewer1 = document.querySelector("#mars-viewer");
 const annotationClicked = (annotation) => {
     let dataset = annotation.dataset;
